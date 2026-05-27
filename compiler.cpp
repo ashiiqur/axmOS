814f255f05ea049cd9c381c413b075d5b15d2582
Ashiqur Rahman <245701478+ashiiqur@users.noreply.github.com>
2026-05-27 12:39:43 +0000

    Add Multiply ...

--- compiler.cpp
+++ compiler.cpp
@@ -6,18 +6,25 @@
 using namespace std;
 
 int main() {
-    cout << "--- axmOS Shell v0.6 ---" << endl;
+
+    string RESET  = "\033[0m";  
+    string RED    = "\033[1;31m"; 
+    string GREEN  = "\033[1;32m"; 
+    string CYAN   = "\033[1;36m"; 
+    string YELLOW = "\033[1;33m"; 
+
+    cout << CYAN << "--- axmOS Shell v0.8 (Full Calculator!) ---" << RESET << endl;
     cout << "Bhasha: Assamese" << endl;
-    cout << "Commands: dekha \"[text]\", jog [num] + [num], safaa, bondho" << endl << endl;
+    cout << "Commands: dekha \"[text]\", hishab [num] [+ - * /] [num], safaa, bondho" << endl << endl;
 
     string axmos_code;
 
     while (true) {
-        cout << "axmOS> ";
+        cout << GREEN << "axmOS> " << RESET;
         getline(cin, axmos_code);
 
         if (axmos_code == "bondho") {
-            cout << "axmOS bondho kori ase... (Shutting down axmOS)" << endl;
+            cout << CYAN << "axmOS bondho kori ase... (Shutting down axmOS)" << RESET << endl;
             break; 
         }
 
@@ -38,30 +45,47 @@ int main() {
                 }
 
                 if (data.empty()) {
-                    cout << "[Bhul/Error]: Ki dekhabo? (What should I show?)" << endl;
+                    cout << RED << "[Bhul/Error]: Ki dekhabo? (What should I show?)" << RESET << endl;
                 } else {
                     cout << data << endl;
                 }
             } 
-            else if (command == "jog") {
+
+            else if (command == "hishab") {
                 int num1, num2;
                 char math_sign; 
                 
                 if (ss >> num1 >> math_sign >> num2) {
                     if (math_sign == '+') {
-                        cout << "Utor (Result): " << (num1 + num2) << endl;
-                    } else {
-                        cout << "[Bhul/Error]: Jog koriboloi '+' chin byobohar korok (Use '+' for addition!)" << endl;
+                        cout << YELLOW << "Uttor (Result): " << (num1 + num2) << RESET << endl;
+                    } 
+                    else if (math_sign == '-') {
+                        cout << YELLOW << "Uttor (Result): " << (num1 - num2) << RESET << endl;
+                    }
+                    else if (math_sign == '*') {
+                        cout << YELLOW << "Uttor (Result): " << (num1 * num2) << RESET << endl;
+                    }
+                    else if (math_sign == '/') {
+
+                        if (num2 == 0) {
+                            cout << RED << "[Bhul/Error]: Shunyo (0) di horon koribo nuwari! (Cannot divide by zero!)" << RESET << endl;
+                        } else {
+                            cout << YELLOW << "Uttor (Result): " << (num1 / num2) << RESET << endl;
+                        }
+                    }
+                    else {
+                        cout << RED << "[Bhul/Error]: Ei chin tu buji napalo (Unknown math symbol). Use +, -, *, /" << RESET << endl;
                     }
                 } else {
-                    cout << "[Bhul/Error]: Sothik format nai! Nirdesh (Example): jog 5 + 5" << endl;
+                    cout << RED << "[Bhul/Error]: Sothik format nai! Nirdesh: hishab 5 + 5" << RESET << endl;
                 }
             }
+
             else if (command == "safaa") {
                 system("clear"); 
             }
             else {
-                cout << "[Bhul/Error]: Ei nirdesh buji napalo (Unknown command): " << command << endl;
+                cout << RED << "[Bhul/Error]: Ei nirdesh buji napalo (Unknown command): " << command << RESET << endl;
             }
         }
     }
